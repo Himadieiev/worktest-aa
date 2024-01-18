dae7b68a95c188655b28b1690ad8ea8a7552a77c
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-18 15:46:04 +0200

    updated files

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/worktest-aa">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
