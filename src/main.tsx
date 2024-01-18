a6eb0595204d38ccf3157274ae8de506cb3435bf
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-18 15:18:23 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/worktest-aa">
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/worktest-aa">
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
